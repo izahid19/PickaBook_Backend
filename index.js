224e454db95002e146b93702503520ef3be5e7c3
zahid mushtaq <mushtaqzahid888@gmail.com>
2025-12-06 05:12:43 +0530

    chore: add cors for production

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://pickabook.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
